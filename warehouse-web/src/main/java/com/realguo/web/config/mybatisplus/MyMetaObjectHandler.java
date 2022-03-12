81da8074093f8eac98e9af1c4678a48316c19558
liuchenchen.ccliu <liuchenchen.ccliu@bytedance.com>
2022-03-12 22:35:31 +0800

    分页正常
    Change-Id: I5eac5469d51ad45bb83ea35aae502587476eb133
    
    分页正常
    
    Change-Id: I5eac5469d51ad45bb83ea35aae502587476eb133

--- warehouse-web/src/main/java/com/realguo/web/config/mybatisplus/MyMetaObjectHandler.java
+++ warehouse-web/src/main/java/com/realguo/web/config/mybatisplus/MyMetaObjectHandler.java
@@ -2,40 +2,39 @@ package com.realguo.web.config.mybatisplus;
 
 import com.baomidou.mybatisplus.mapper.MetaObjectHandler;
 import com.realguo.web.config.shiro.ShiroUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.reflection.MetaObject;
 import org.apache.shiro.UnavailableSecurityManagerException;
-import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
-@Component
+@Slf4j
 public class MyMetaObjectHandler extends MetaObjectHandler {
 
-	@Override
-	public void insertFill(MetaObject metaObject) {
-//		log.info("新增的时候干点不可描述的事情");
-		setFieldValByName("createTime", new Date(), metaObject);
-		try {
-			if (ShiroUtils.getUserEntity() != null) {
-				setFieldValByName("createBy", ShiroUtils.getUserEntity().getUsername(), metaObject);
-			}
-		} catch (UnavailableSecurityManagerException e) {
-			System.out.println("定时任务不能由shiro管理，所以执行shiro方法会报错");
-		}
-	}
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        log.info("新增的时候干点不可描述的事情");
+        setFieldValByName("createTime", new Date(), metaObject);
+        try {
+            if (ShiroUtils.getUserEntity() != null) {
+                setFieldValByName("createBy", ShiroUtils.getUserEntity().getUsername(), metaObject);
+            }
+        } catch (UnavailableSecurityManagerException e) {
+            System.out.println("定时任务不能由shiro管理，所以执行shiro方法会报错");
+        }
+    }
 
-	@Override
-	public void updateFill(MetaObject metaObject) {
-//		log.info("更新的时候干点不可描述的事情");
-
-		setFieldValByName("updateTime", new Date(), metaObject);
-		try {
-			if (ShiroUtils.getUserEntity() != null) {
-				setFieldValByName("updateBy", ShiroUtils.getUserEntity().getUsername(), metaObject);
-			}
-		} catch (UnavailableSecurityManagerException e) {
-			System.out.println("定时任务不能由shiro管理,所以执行shiro方法会报错");
-		}
-	}
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        log.info("更新的时候干点不可描述的事情");
+        setFieldValByName("updateTime", new Date(), metaObject);
+        try {
+            if (ShiroUtils.getUserEntity() != null) {
+                setFieldValByName("updateBy", ShiroUtils.getUserEntity().getUsername(), metaObject);
+            }
+        } catch (UnavailableSecurityManagerException e) {
+            System.out.println("定时任务不能由shiro管理,所以执行shiro方法会报错");
+        }
+    }
 
 }
--- warehouse-web/src/main/java/com/realguo/web/config/mybatisplus/MybatisPlusConfig.java
+++ warehouse-web/src/main/java/com/realguo/web/config/mybatisplus/MybatisPlusConfig.java
@@ -2,10 +2,11 @@ package com.realguo.web.config.mybatisplus;
 
 import com.baomidou.mybatisplus.entity.GlobalConfiguration;
 import com.baomidou.mybatisplus.mapper.MetaObjectHandler;
-import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
 import com.baomidou.mybatisplus.spring.MybatisSqlSessionFactoryBean;
 import com.baomidou.mybatisplus.toolkit.GlobalConfigUtils;
 import com.realguo.web.config.mybatisplus.MyMetaObjectHandler;
+import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
+import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,29 +22,19 @@ import javax.sql.DataSource;
 @Configuration
 public class MybatisPlusConfig {
 
-    /**
-     * 分页插件
-     */
     @Bean
-    public PaginationInterceptor paginationInterceptor() {
-        return new PaginationInterceptor();
-    }
-
-    @Bean
-    public MetaObjectHandler metaObjectHandler() {
-        return new MyMetaObjectHandler();
-    }
-
-    @Bean
-    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+    public MybatisSqlSessionFactoryBean sqlSessionFactory(DataSource dataSource) throws Exception {
         MybatisSqlSessionFactoryBean mybatisSqlSessionFactoryBean = new MybatisSqlSessionFactoryBean();
-        //获取mybatis-plus全局配置
+        // 获取mybatis-plus全局配置
         GlobalConfiguration globalConfig = GlobalConfigUtils.defaults();
-        //mybatis-plus全局配置设置元数据对象处理器为自己实现的那个
+        // mybatis-plus全局配置设置元数据对象处理器为自己实现的那个(自动填充)
         globalConfig.setMetaObjectHandler(new MyMetaObjectHandler());
         mybatisSqlSessionFactoryBean.setDataSource(dataSource);
-        //mybatisSqlSessionFactoryBean关联设置全局配置
+        // mybatisSqlSessionFactoryBean关联设置全局配置
         mybatisSqlSessionFactoryBean.setGlobalConfig(globalConfig);
-        return mybatisSqlSessionFactoryBean.getObject();
+        // 分页插件
+        Interceptor[] plugins = new Interceptor[]{new PaginationInterceptor()};
+        mybatisSqlSessionFactoryBean.setPlugins(plugins);
+        return mybatisSqlSessionFactoryBean;
     }
 }
