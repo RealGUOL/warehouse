36e3bc857f511254b78a948a14c75a0b7e6bc8ae
liuchenchen.ccliu <liuchenchen.ccliu@bytedance.com>
2022-03-30 22:49:17 +0800

    添加bridge网络配置
    Change-Id: I97c155b1eb5a0c065fba8e5ddab8c65b12f1a743

--- warehouse-web/build.sh
+++ warehouse-web/build.sh
@@ -1,6 +1,7 @@
 cd /root/warehouse-web
+docker network create warehouse-net
 docker stop warehouse
 docker container rm warehouse
 docker image rm warehouse
 docker build -t warehouse .
-docker run -d --name warehouse -p 8080:8080 warehouse
+docker run -d --name warehouse -p 8080:8080 --network warehouse-net --network-alias warehouse  warehouse
